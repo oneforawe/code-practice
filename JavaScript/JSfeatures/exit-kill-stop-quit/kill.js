6d2dddf135bab2bf135920de6b3984578d5cebdf
Andrew Forrester <awforrester@gmail.com>
2021-06-10 14:29:52 -0700

    Add new JavaScript feature exploration: exit-kill-stop-quit.

---
+++ JavaScript/JSfeatures/exit-kill-stop-quit/kill.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+/*
+ * Filename: kill.js
+ * Language: JavaScript / ECMAScript
+ * Author:   Andrew Forrester <andrew@andrew-forrester.com>
+ *
+ * Basic Usage: Execute in a shell `node kill.js`
+ *
+ * Reference: https://nodejs.dev/learn/how-to-exit-from-a-nodejs-program
+*/
+
+console.log('The program/process is running.')
+console.log('Note that when killed, the process will return an exit code of 143, which will (in some contexts) yield error messages.')
+
+process.kill(process.pid, 'SIGTERM')
